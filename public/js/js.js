d78fc481d8cbf3def2f9417f176faf59d28904a6
Bauer Sandro <ga65bam@mytum.de>
2018-08-20 20:55:38 +0200

    Played with transitions, more recipes

--- public/js/js.js
+++ public/js/js.js
@@ -10,4 +10,78 @@ function getSecondClass(elem) {
     return '.' + $(elem).attr('class').split(' ')[1]
 }
 
+function isMD() {
+    let xs = window.getComputedStyle(document.documentElement).getPropertyValue('--breakpoint-xs');
+    let sm = window.getComputedStyle(document.documentElement).getPropertyValue('--breakpoint-sm');
+    let md = window.getComputedStyle(document.documentElement).getPropertyValue('--breakpoint-md');
+    let lg = window.getComputedStyle(document.documentElement).getPropertyValue('--breakpoint-lg');
+    let xl = window.getComputedStyle(document.documentElement).getPropertyValue('--breakpoint-xl');
+    let is_xs = window.matchMedia('(min-width: '+xs+')').matches;
+    let is_sm = window.matchMedia('(min-width: '+sm+')').matches;
+    let is_md = window.matchMedia('(min-width: '+md+')').matches;
+    let is_lg = window.matchMedia('(min-width: '+lg+')').matches;
+    let is_xl = window.matchMedia('(min-width: '+xl+')').matches;
+    if (is_xl) {
+        return true;
+    }
+    if (is_lg) {
+        return true;
+    }
+    if (is_md) {
+        return true;
+    }
+    return false;
+}
+
+isMD();
+
+function fadeOutIngredients() {
+    if ($('#ingredients').hasClass('d-none')) {
+        return;
+    }
+    if (isMD()) {
+        setTimeout(() => {
+            $('#ingredients').toggleClass('col-md-4 col-md-1 grow shrink');
+            $('#preparation').toggleClass('col-md-8 col-md-11 grow shrink');
+        }, 100);
+        setTimeout(() => {
+            $('#ingredients').addClass('d-none');
+            $('#ingredients-vert').removeClass('d-none');
+            $('#preparation').addClass('d-none');
+            $('#preparation-amounts').removeClass('d-none');
+        }, 1100);
+    } else {
+        $('#inglist').toggleClass('h-0 h-100');
+        setTimeout(() => {
+            if ($('#preparation').hasClass('d-none')) {
+                $('#preparation-amounts').addClass('d-none');
+                $('#preparation').removeClass('d-none');
+            } else {
+                $('#preparation').addClass('d-none');
+                $('#preparation-amounts').removeClass('d-none');
+            }
+        }, 100);
+    }
+    setTimeout(() => {
+        $('#preparation').addClass('d-none');
+        $('#preparation-amounts').removeClass('d-none');
+    }, 1200);
+}
+
+function fadeInIngredients() {
+    if (!$('#ingredients').hasClass('d-none')) {
+        return;
+    }
+    $('#preparation').removeClass('d-none');
+    $('#preparation-amounts').addClass('d-none');
+    if (isMD()) {
+        $('#ingredients-vert').addClass('d-none');
+        $('#ingredients').removeClass('d-none');
+        setTimeout(() => {
+            $('#ingredients').toggleClass('col-md-4 col-md-1 grow shrink');
+            $('#preparation').toggleClass('col-md-8 col-md-11 grow shrink');
+        }, 100);
+    }
+}
+
 $('img').Lazy();
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 let fs = require('fs');
-let recipes = JSON.parse(fs.readFileSync('rezepte.json', 'utf8'));
+let recipes = loadJSON();
 app.set('view engine', 'pug');
 app.use(express.static('public'));
 
@@ -14,7 +14,12 @@ app.get('/', (req, res) => {
     });
 });
 
-app.get('/desert/:recipe',function(req,res) {
+app.get('/reload-json', (req, res) => {
+    recipes = loadJSON();
+    res.redirect('/');
+});
+
+app.get('/desert/:recipe', (req,res) => {
     recipe = findById(recipes.deserts, req.params.recipe);
     if (recipe) {
         res.render('recipe', recipe);
@@ -23,7 +28,7 @@ app.get('/desert/:recipe',function(req,res) {
     }
 });
 
-app.get('/mains/:recipe',function(req,res) {
+app.get('/mains/:recipe', (req,res) => {
     recipe = findById(recipes.mains, req.params.recipe);
     if (recipe) {
         res.render('recipe', recipe);
@@ -32,13 +37,23 @@ app.get('/mains/:recipe',function(req,res) {
     }
 });
 
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err);
+    }
+    console.log(`server is listening on ${port}`);
+});
+
+
 function findById(arr, id) {
     for (let i = 0; i < arr.length; i++) {
         recipe = arr[i];
         if (recipe.id == id) {
+            prep = formatPreparation(recipe);
             return {
                 ingredients: recipe.ingredients,
-                preparation: formatPreparation(recipe),
+                preparation: prep[0],
+                preparationAmounts: prep[1],
                 images: recipe.image,
                 name: recipe.name,
                 portions: recipe.portions
@@ -48,21 +63,23 @@ function findById(arr, id) {
     return null;
 }
 
-app.listen(port, (err) => {
-    if (err) {
-        return console.log('something bad happened', err);
-    }
-
-    console.log(`server is listening on ${port}`);
-});
-
 function formatPreparation(recipe) {
     // replace preparations
     preparation = JSON.stringify(recipe.preparation);
+    preparationAmounts = JSON.stringify(recipe.preparation);
     recipe.ingredients.forEach(ingredient => {
         let regex = `\{${ingredient.id}\}`;
-        let replace = '<b class=\'ingredient ' + ingredient.id + '\'>' + /*(ingredient.amount ? ingredient.amount + ' ' : '') +*/ ingredient.name + '</b>';
+        let replace = '<b class=\'ingredient ' + ingredient.id + '\'>' + ingredient.name + '</b>';
+        let replaceAmounts = '<b class=\'ingredient ' + ingredient.id + '\'>' + (ingredient.amount ? ingredient.amount + ' ' : '') + ingredient.name + '</b>';
         preparation = preparation.replace(new RegExp(regex, 'g'), replace);
+        preparationAmounts = preparationAmounts.replace(new RegExp(regex, 'g'), replaceAmounts);
     });
-    return JSON.parse(preparation);
+    return [ 
+        JSON.parse(preparation),
+        JSON.parse(preparationAmounts)
+    ];
+}
+
+function loadJSON() {
+    return JSON.parse(fs.readFileSync('rezepte.json', 'utf8'));
 }
