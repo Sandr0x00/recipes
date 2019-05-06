f1e67a4ad674a3006dd46f65d282ae90e0b65494
Sandr0x00 <sandro.bauer.93@gmail.com>
2019-05-06 13:00:37 +0200

    Fixed deployment

--- make_dist.py
+++ make_dist.py
@@ -8,11 +8,13 @@ if __name__ == "__main__":
     if not os.path.isdir(folder):
         os.mkdir(folder)
     else:
-        shutil.rmtree(dist) 
+        shutil.rmtree(folder)
         os.mkdir(folder)
 
     shutil.copy('helper.js', 'dist')
     shutil.copy('server.js', 'dist')
+    shutil.copy('package.json', 'dist')
+    shutil.copy('package-lock.json', 'dist')
     shutil.copytree('public', 'dist/public')
     shutil.copytree('views', 'dist/views')
     shutil.copytree('recipes', 'dist/recipes')
